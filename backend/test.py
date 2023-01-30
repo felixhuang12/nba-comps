90e7579b0315c170e3f4b6e0eed3855830e70cff
Felix Huang <felixhuang12@gmail.com>
2023-01-30 00:45:53 -0500

    reorganization and configs

---
+++ backend/test.py
@@ -0,0 +1,8 @@
+from nba_api.stats.endpoints import playercareerstats
+
+# Nikola Jokić
+career = playercareerstats.PlayerCareerStats(player_id='203999') 
+
+# pandas data frames (optional: pip install pandas)
+df = career.get_data_frames()[0]
+print(df)
